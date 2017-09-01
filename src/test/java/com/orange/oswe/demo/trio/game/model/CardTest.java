7c8f12ba1c6a84eb18febaea5d27734a18e4e7f1
Pierre Smeyers <pierre.smeyers@orange.com>
2017-09-01 11:57:01 +0200

    AssertJ tests

--- src/test/java/com/orange/oswe/demo/trio/game/model/CardTest.java
+++ src/test/java/com/orange/oswe/demo/trio/game/model/CardTest.java
@@ -2,7 +2,7 @@ package com.orange.oswe.demo.trio.game.model;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by crhx7117 on 26/06/17.
@@ -11,10 +11,10 @@ public class CardTest {
     @Test
     public void card_creator() {
         Card card = new Card(0, 1, 2, 0);
-        assertEquals(0, card.getAttribute(Card.Attribute.color));
-        assertEquals(1, card.getAttribute(Card.Attribute.shape));
-        assertEquals(2, card.getAttribute(Card.Attribute.fill));
-        assertEquals(0, card.getAttribute(Card.Attribute.number));
+        assertThat(card.getAttribute(Card.Attribute.color)).isEqualTo(0);
+        assertThat(card.getAttribute(Card.Attribute.shape)).isEqualTo(1);
+        assertThat(card.getAttribute(Card.Attribute.fill)).isEqualTo(2);
+        assertThat(card.getAttribute(Card.Attribute.number)).isEqualTo(0);
     }
 
     @Test
@@ -22,7 +22,7 @@ public class CardTest {
         Card card1 = new Card(0, 0, 1, 2);
         Card card2 = new Card(1, 0, 1, 2);
         Card card3 = new Card(2, 0, 1, 0);
-        assertEquals(Card.Attribute.number, Card.isTrio(card1, card2, card3));
+        assertThat(Card.isTrio(card1, card2, card3)).containsExactlyInAnyOrder(Card.Attribute.number);
     }
 
     @Test
@@ -30,7 +30,7 @@ public class CardTest {
         Card card1 = new Card(0, 0, 1, 2);
         Card card2 = new Card(1, 0, 1, 1);
         Card card3 = new Card(2, 0, 1, 0);
-        assertNull(Card.isTrio(card1, card2, card3));
+        assertThat(Card.isTrio(card1, card2, card3)).isEmpty();
     }
 
 }
